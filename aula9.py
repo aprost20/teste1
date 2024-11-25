022fe43343d1953a1505075b1244554afcfbd361
aprost20 <ana.rost@planejamento.gov.br>
2024-11-25 11:25:16 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 df = pd.DataFrame({'nomeServidor': ['ana', 'aline', 'adriana'], 'idade': [40, 41, 43]})
-df
+
 st.write('Criando uma tabela')
 st.write(df)
