7e8eca9d2623324d273a0268ae4e20e66c8d3fd7
aprost20 <ana.rost@planejamento.gov.br>
2024-11-25 11:29:36 -0300

    Update aula9.py

--- aula9.py
+++ aula9.py
@@ -4,3 +4,5 @@ df = pd.DataFrame({'nomeServidor': ['ana', 'aline', 'adriana'], 'idade': [40, 41
 
 st.write('Criando uma tabela')
 st.write(df)
+
+st.selectbox('Qual servidor vc gostaria de selecionar?', df['nomeServidor'])
