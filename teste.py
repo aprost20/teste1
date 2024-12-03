a61ae27a8aabf5d0d80f22e505d5ba2cf3b1ba9a
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 17:19:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import streamlit as st
 import io
-import requests
+import requests as rq
 
 st.title('Demandas TCU recebidas pelo MPO em 2024')
 
+# Get the raw file content from GitHub using the 'raw' subdomain
 url = 'https://raw.githubusercontent.com/aprost20/teste1/main/Demandas2024.xlsx'
 response = requests.get(url)
-response.raise_for_status() 
+response.raise_for_status()  # Raise an exception for bad responses
 
+# Read the Excel file from the response content using io.BytesIO
 df = pd.read_excel(io.BytesIO(response.content), engine='openpyxl') 
 
 st.write("Demandas TCU 2024")
