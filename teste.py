a8ea18a0f2a99adf6b3fd95044c9fad63e1f8441
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 18:02:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -7,16 +7,7 @@ import requests as rq
 st.title('Demandas TCU recebidas pelo MPO em 2024')
 st.write("Demandas TCU 2024")
 
-# URL do arquivo Excel no GitHub
-url = 'https://raw.githubusercontent.com/aprost20/teste1/main/Demandas2024.xlsx'
 
-# Carregar o arquivo do GitHub
-try:
-    response = rq.get(url)
-    response.raise_for_status()  # Verifica se a requisição foi bem-sucedida
-
-    # Ler o conteúdo do Excel
-    df = pd.read_excel(io.BytesIO(response.content))
 
  
 
