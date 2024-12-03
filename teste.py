148a5a5fc286ba770059526e1e5deb620ea1b6fb
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 15:40:20 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,3 +4,4 @@ import streamlit as st
 
 st.set_page_config(page_title = "Demandas TCU 2024", layout = "wide")
 df = pd.read_excel('/content/Demandas2024.xlsx', sheet_name = 0)
+st.title("Demandas TCU recebidas pelo MPO em 2024")
