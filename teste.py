5a36fe52ce349cf5284137174424105b65d9f8d6
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 18:03:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,6 +9,14 @@ st.title('Demandas TCU recebidas pelo MPO em 2024')
 # URL do arquivo Excel no GitHub
 url = 'https://raw.githubusercontent.com/aprost20/teste1/main/Demandas2024.xlsx'
 
+fig1, fig2 = st.columns(2)
+
+with fig1:
+  quantidades = df['estado_SISCOD'].value_counts()
+  fig1 = px.bar(quantidades, text_auto = True, orientation = 'h')
+  fig1.update_layout(title = 'Demandas TCU x Situação', xaxis_title = "Quantidade de itens demandados", yaxis_title = "Situação")
+  st.plotly_chart(fig1, use_container_width = True)
+
 
 
 
