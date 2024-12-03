d3a331b8253441cf2562ee0d24372eb8e7e12c5f
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 16:10:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,6 +5,4 @@ import streamlit as st
 st.set_page_config(page_title = "Demandas TCU 2024", layout = "wide")
 st.title("Demandas TCU recebidas pelo MPO em 2024")
 
-urldemandas = "https://github.com/aprost20/teste1/blob/main/Demandas2024.xlsx"
-resposta = req.get(urldemandas)
-dadosJSON = resposta.json()
+df = pd.read_excel('/content/Demandas2024.xlsx')
