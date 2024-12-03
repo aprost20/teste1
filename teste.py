0b8c3d885ff32fd0a95028ec884b5069cabe05bb
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 18:03:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,7 +5,11 @@ import requests as rq
 
 # Título do dashboard
 st.title('Demandas TCU recebidas pelo MPO em 2024')
-st.write("Demandas TCU 2024")
+
+# URL do arquivo Excel no GitHub
+url = 'https://raw.githubusercontent.com/aprost20/teste1/main/Demandas2024.xlsx'
+
+
 
 
 
