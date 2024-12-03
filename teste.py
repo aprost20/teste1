e43f5085b2ee3b2173c529e29861cb1e8ffc622f
aprost20 <ana.rost@planejamento.gov.br>
2024-12-03 20:17:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,7 +8,14 @@ st.title('Demandas TCU recebidas pelo MPO em 2024')
 
 def get_data_from_excel():
  df = pd.read_excel( io = '/content/Demandas2024.xlsx', engine = 'openpyxl', sheet_name = 'None', usecols = 'A:Z', nrows = 262)
-st.write(df)
+
+fig1, fig2 = st.columns(2)
+
+with fig1:
+  quantidades = df['estado_SISCOD'].value_counts()
+  fig1 = px.bar(quantidades, text_auto = True, orientation = 'h')
+  fig1.update_layout(title = 'Demandas TCU x Situação', xaxis_title = "Quantidade de itens demandados", yaxis_title = "Situação")
+  st.plotly_chart(fig1, use_container_width = True)
 
 
 
