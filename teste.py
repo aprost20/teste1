a61f4e339267eaf21e5d04b8e7e1c016c306f195
aprost20 <ana.rost@planejamento.gov.br>
2024-12-04 08:57:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,41 +1,17 @@
 import pandas as pd
-import plotly.express as px
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(page_title="Demandas TCU 2024", layout="wide")
 
-# Carregamento do arquivo Excel
-df = pd.read_excel('Demandas2024.xlsx', sheet_name=0)
+# Carregar arquivo via upload
+uploaded_file = st.file_uploader("Carregue o arquivo Demandas2024.xlsx", type="xlsx")
 
-st.title("Demandas TCU recebidas pelo MPO em 2024")
-
-# Gráfico 1 e 2
-col1, col2 = st.columns(2)
-
-with col1:
-    quantidades = df['estado_SISCOD'].value_counts()
-    fig1 = px.bar(quantidades, text_auto=True, orientation='h')
-    fig1.update_layout(
-        title='Demandas TCU x Situação',
-        xaxis_title="Quantidade de itens demandados",
-        yaxis_title="Situação"
-    )
-    st.plotly_chart(fig1, use_container_width=True)
-
-with col2:
-    tipo_demanda = df['tipo_processo'].value_counts()
-    fig2 = px.bar(tipo_demanda, text_auto=True)
-    fig2.update_layout(
-        title='Ofícios TCU 2024',
-        xaxis_title="Tipo de Processo TCU",
-        yaxis_title="Quantidade Ofícios recebidos"
-    )
-    st.plotly_chart(fig2, use_container_width=True)
-  
-
-
-
-
- 
+if uploaded_file:
+    df = pd.read_excel(uploaded_file, sheet_name=0)
+    st.title("Demandas TCU recebidas pelo MPO em 2024")
+    # Continue com o restante do código...
+else:
+    st.warning("Por favor, carregue um arquivo para visualizar os dados.")
 
    
