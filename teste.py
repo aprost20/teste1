c9fa835510c0abff2ca9c153627b1c2cf66b0d97
aprost20 <ana.rost@planejamento.gov.br>
2024-12-04 09:10:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,27 +9,4 @@ df = pd.read_excel('Demandas2024.xlsx', sheet_name=0)
 
 st.title("Demandas TCU recebidas pelo MPO em 2024")
 
-# Gráfico 1 e 2
-col1, col2 = st.columns(2)
 
-with col1:
-    quantidades = df['estado_SISCOD'].value_counts()
-    fig1 = px.bar(quantidades, text_auto=True, orientation='h')
-    fig1.update_layout(
-        title='Demandas TCU x Situação',
-        xaxis_title="Quantidade de itens demandados",
-        yaxis_title="Situação"
-    )
-    st.plotly_chart(fig1, use_container_width=True)
-
-with col2:
-    tipo_demanda = df['tipo_processo'].value_counts()
-    fig2 = px.bar(tipo_demanda, text_auto=True)
-    fig2.update_layout(
-        title='Ofícios TCU 2024',
-        xaxis_title="Tipo de Processo TCU",
-        yaxis_title="Quantidade Ofícios recebidos"
-    )
-    st.plotly_chart(fig2, use_container_width=True)
-
-   
