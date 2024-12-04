f4654d4c375749fc512faf4ed1dcd7bf3433145a
aprost20 <ana.rost@planejamento.gov.br>
2024-12-04 16:19:35 -0300

    Update demandasTCU2024.py

--- demandasTCU2024.py
+++ demandasTCU2024.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     quantidades = df['estado_SISCOD'].value_counts()
-    fig1 = px.bar(quantidades, text_auto=True, orientation='h', color_discrete_sequence=['#FF5733', '#33FF57'])
+    fig1 = px.bar(quantidades, text_auto=True, orientation='h', color_discrete_sequence=['#33FF57'])
     fig1.update_layout(
         title='Demandas TCU x Situação',
         xaxis_title="Quantidade de itens demandados",
