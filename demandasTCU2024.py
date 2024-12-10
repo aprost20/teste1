fc2a0327e0f4cb5f2e125c99775ae095c7d61ed1
aprost20 <ana.rost@planejamento.gov.br>
2024-12-10 17:41:34 -0300

    Update demandasTCU2024.py

--- demandasTCU2024.py
+++ demandasTCU2024.py
@@ -67,7 +67,8 @@ with col6:
 col7, col8 = st.columns(2)
 
 with col17:
-  fig7 = px.pie(df, values = 'valor_no_prazo', names = 'no_prazo')
+  no_prazo_counts = df['no_prazo'].value_counts()
+  fig7 = px.pie(no_prazo_counts, values = 'valor_no_prazo', names = 'no_prazo')
   fig7.update_layout(title = 'Atendimento às demandas quanto ao prazo')
   st.plotly_chart(fig7, use_container_width = True)
 
