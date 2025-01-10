6e2207799cd86e30b2b138415a3f8f1e4d17e975
aprost20 <ana.rost@planejamento.gov.br>
2025-01-10 17:56:45 -0300

    Update demandasTCU2024.py

--- demandasTCU2024.py
+++ demandasTCU2024.py
@@ -74,7 +74,7 @@ with col7:
 
 with col8:
   atend_acordao = df2.query('Ato == "Determinação" or Ato == "Recomendação"')
-  fig8 = px.bar(atend_acordao, x = 'ano_acordao', y = 'valor_ano', color = 'Ato', text_auto = True, color_discrete_sequence=['#2237FF'])  
+  fig8 = px.bar(atend_acordao, x = 'ano_acordao', y = 'valor_ano', color = 'Ato', text_auto = True)  
   fig8.update_layout(title = 'Deliberações implementadas pelo MPO desde o início da gestão', xaxis_title = "Ano da deliberação", yaxis_title = "Quantidade de itens")
   st.plotly_chart(fig8, use_container_width = True)
 
