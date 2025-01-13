ab3fdc8fcd9719532d6fc983b463e658c3d98aa9
aprost20 <ana.rost@planejamento.gov.br>
2025-01-13 17:10:19 -0300

    Update demandasTCU2024.py

--- demandasTCU2024.py
+++ demandasTCU2024.py
@@ -16,7 +16,7 @@ with col2:
 
 col1, col2 = st.columns([3,1])
 with col1:
-st.title("Demandas TCU recebidas pelo MPO em 2024")
+    st.title("Demandas TCU recebidas pelo MPO em 2024")
 
 st.markdown("---")
 
